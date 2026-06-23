b69fccf5fa6e1b2cbef58f62779371c389c1fe8c
st1ngg1 <sfh126@mail.ru>
2026-06-23 18:57:06 +0300

    4.9 Задание 12. Инверсия контроля. Внедрение зависимостей

--- task11_maven/src/main/java/org/example/Main.java
+++ task11_maven/src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-// Импортируем наши классы из пакета task12_ioc
+
 import org.example.task12_ioc.*;
 
 import java.util.ArrayList;
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
     private final List<MathOperation> operations;
 
-    // Конструктор принимает список операций (Внедрение зависимостей)
+
     public Main(List<MathOperation> operations) {
         this.operations = operations;
     }
@@ -24,7 +24,7 @@ public class Main {
         System.out.println("Введите число b");
         double b = scanner.nextDouble();
 
-        // Проходим по всем операциям и выводим результат
+
         for (MathOperation op : operations) {
             double result = op.execute(a, b);
             System.out.printf("Результат %s a и b: %.1f%n", op.getName(), result);
@@ -33,17 +33,17 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Создаем список и добавляем туда все нужные операции
+
         List<MathOperation> ops = new ArrayList<>();
         ops.add(new Adder());
         ops.add(new Subtractor());
         ops.add(new Multiplier());
         ops.add(new Divider());
 
-        // Передаем список в конструктор (DI)
+
         Main app = new Main(ops);
 
-        // Запускаем
+
         app.run();
     }
 }
