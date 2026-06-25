9b11a0bfb4df8e09bb8d2b9243225235ab0347ef
st1ngg1 <sfh126@mail.ru>
2026-06-25 04:17:00 +0300

    4.4 Задание 9. расширить функционал класса Student

--- task09 4.4/src/main/java/StudentWithClock.java
+++ task09 4.4/src/main/java/StudentWithClock.java
@@ -1,26 +1,25 @@
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public class StudentWithClock {
-    private String name;
-    
-    public StudentWithClock(String name) {
-        this.name = name;
+public class StudentWithClock implements Learner {
+    private final Learner learner;
+
+    public StudentWithClock(Learner learner) {
+        this.learner = learner;
     }
-    
+
+    @Override
     public void learn() {
-        System.out.println("Я учусь. .zZ");
-        
+        learner.learn();
+
         LocalTime currentTime = LocalTime.now();
-        
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        String formattedTime = currentTime.format(formatter);
-        System.out.println("Я закончил учиться");
-        System.out.println("Текущее время: " + formattedTime);
+        System.out.println("Текущее время: " + currentTime.format(formatter));
     }
-    
+
     public static void main(String[] args) {
-        StudentWithClock student = new StudentWithClock("Студент");
+
+        Learner student = new StudentWithClock(new Student());
         student.learn();
     }
 }
